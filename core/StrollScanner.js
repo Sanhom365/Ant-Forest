aeaeab8c2db5592124d4f8b85183ee9c711fe91b
Sanhom <58111416+Sanhom365@users.noreply.github.com>
2026-08-13 14:47:35 +0800

    适配了2026年蚂蚁森林更新后的逛一逛及好友能量收取（测试中）

--- core/StrollScanner.js
+++ core/StrollScanner.js
@@ -200,7 +200,7 @@ StrollScanner.prototype.collectTargetFriend = function () {
   }
   // 直接判断是否出现了逛完了的标志，替代原先会超时的 alternativeWidget 方法
   // 保留了 _config.stroll_end_ui_content || /找能量共获得.*/ 的判断
-  let isStrollEnd = ((alternativeFriendOrDone = _widgetUtils.alternativeWidget(_config.friend_home_check_regex, _config.stroll_end_ui_content || /找能量共获得.*/, null, false, null, { algorithm: 'PVDFS' })) !== 1)
+  let isStrollEnd = ((alternativeFriendOrDone = _widgetUtils.alternativeWidget(_config.friend_home_check_regex, _config.stroll_end_ui_content || /找能量共获得.*/, 500, false, null, { algorithm: 'PVDFS' })) !== 1)
   if (isStrollEnd) {
         let ended = false
     if (alternativeFriendOrDone === 2) {
